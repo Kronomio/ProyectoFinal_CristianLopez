c0ea9215bb471a1f5696a4f3c8bf75b4885a532b
Cristian López <cristianlp.ok@gmail.com>
2022-05-24 17:44:34 -0300

    creacion de componentes de formacion e items

---
+++ ProyectoFinal_CristianLopez/src/app/componentes/item-formacion/item-formacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-formacion',
+  templateUrl: './item-formacion.component.html',
+  styleUrls: ['./item-formacion.component.css']
+})
+export class ItemFormacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
