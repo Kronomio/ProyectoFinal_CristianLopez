b04e36134ada52442b0d8961e61fabdd924c79f0
Cristian López <cristianlp.ok@gmail.com>
2022-06-29 12:07:32 -0300

    configuracion BackEnd

---
+++ ProyectoFinal_CristianLopez/Back-End/src/test/java/com/portfolio/CristianLopez/CristianLopezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.CristianLopez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CristianLopezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
