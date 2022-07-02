c98b16b5095cc244baba2b21e45a20cc608e4306
Cristian López <cristianlp.ok@gmail.com>
2022-07-02 17:49:30 -0300

    Configuraciones generales

--- ProyectoFinal_CristianLopez/Back-End/src/main/java/com/portfolio/CristianLopez/CristianLopezApplication.java
+++ ProyectoFinal_CristianLopez/Back-End/src/main/java/com/portfolio/CristianLopez/CristianLopezApplication.java
@@ -3,9 +3,11 @@ package com.portfolio.CristianLopez;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CristianLopezApplication {
 
+   
 	public static void main(String[] args) {
 		SpringApplication.run(CristianLopezApplication.class, args);
 	}
--- ProyectoFinal_CristianLopez/Back-End/src/test/java/com/portfolio/CristianLopez/CristianLopezApplicationTests.java
+++ ProyectoFinal_CristianLopez/Back-End/src/test/java/com/portfolio/CristianLopez/CristianLopezApplicationTests.java
@@ -1,12 +1,11 @@
 package com.portfolio.CristianLopez;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+
 class CristianLopezApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
