e8482c58784d7f88d1d78ca5d738a2992aeb8735
Cristian López <cristianlp.ok@gmail.com>
2022-07-11 15:20:37 -0300

    Cambios de colores

--- ProyectoFinal_CristianLopez/Front-End/src/app/app.module.ts
+++ ProyectoFinal_CristianLopez/Front-End/src/app/app.module.ts
@@ -7,20 +7,17 @@ import { PortadaComponent } from './componentes/portada/portada.component';
 import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 import { FormacionComponent } from './componentes/formacion/formacion.component';
 import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
-import { ItemHabilidadesComponent } from './componentes/item-habilidades/item-habilidades.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { ContactoComponent } from './componentes/contacto/contacto.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
-import { ItemProyectosComponent } from './componentes/item-proyectos/item-proyectos.component';
 import { ExperienciaLaboralComponent } from './componentes/experiencia-laboral/experiencia-laboral.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { HomeComponent } from './componentes/home/home.component';
-import { ItemExperienciaComponent } from './componentes/item-experiencia/item-experiencia.component';
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome'
-import {  InterceptorService } from './services/interceptor-service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { InterceptorService } from './services/interceptor-service';
 import { SignupComponent } from './componentes/signup/signup.component';
 @NgModule({
   declarations: [
@@ -30,18 +27,13 @@ import { SignupComponent } from './componentes/signup/signup.component';
     AcercaDeComponent,
     FormacionComponent,
     HabilidadesComponent,
-    ItemHabilidadesComponent,
     FooterComponent,
     ContactoComponent,
     ProyectosComponent,
-    ItemProyectosComponent,
     ExperienciaLaboralComponent,
     LoginComponent,
     HomeComponent,
-    ItemExperienciaComponent,
     SignupComponent
- 
-    
   ],
   imports: [
     BrowserModule,
@@ -58,15 +50,15 @@ import { SignupComponent } from './componentes/signup/signup.component';
       outerStrokeColor: "#78C000",
       innerStrokeColor: "#C7E596",
       animationDuration: 30000
-      
+
     })
-    
+
   ],
   providers: [
     {
-    provide: HTTP_INTERCEPTORS,
-    useClass: InterceptorService,
-    multi: true
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptorService,
+      multi: true
     }
   ],
   bootstrap: [AppComponent]
