7cabcdece56c3252f9582368a371dcd775296483
Cristian López <cristianlp.ok@gmail.com>
2022-09-12 21:21:51 -0300

    Nueva implementación

--- ProyectoFinal_CristianLopez/Front-End/src/app/app.module.ts
+++ ProyectoFinal_CristianLopez/Front-End/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 import { FormacionComponent } from './componentes/formacion/formacion.component';
 import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
 import { FooterComponent } from './componentes/footer/footer.component';
-import { ContactoComponent } from './componentes/contacto/contacto.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 import { ExperienciaLaboralComponent } from './componentes/experiencia-laboral/experiencia-laboral.component';
@@ -17,12 +16,11 @@ import { HomeComponent } from './componentes/home/home.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
-import { InterceptorService } from './services/interceptor-service';
+import { InterceptorService } from './services/interceptor.service';
 import { SignupComponent } from './componentes/signup/signup.component';
 import { NgToastModule } from 'ng-angular-popup';
 import { UsuariosComponent } from './componentes/usuarios/usuarios.component';
 import { ChangePasswordComponent } from './componentes/change-password/change-password.component';
-import { ConfiguracionesComponent } from './componentes/configuraciones/configuraciones.component';
 import { SpinnerComponent } from './utils/spinner/spinner.component'
 import { SpinnerInterceptor } from './utils/spinner/spinner.interceptor';
 import { SocialBarFloatingComponent } from './utils/social-bar-floating/social-bar-floating.component';
@@ -38,7 +36,6 @@ import { SocialBarFloatingComponent } from './utils/social-bar-floating/social-b
     FormacionComponent,
     HabilidadesComponent,
     FooterComponent,
-    ContactoComponent,
     ProyectosComponent,
     ExperienciaLaboralComponent,
     LoginComponent,
@@ -46,10 +43,9 @@ import { SocialBarFloatingComponent } from './utils/social-bar-floating/social-b
     SignupComponent,
     UsuariosComponent,
     ChangePasswordComponent,
-    ConfiguracionesComponent,
     SpinnerComponent,
     SocialBarFloatingComponent
-  
+
 
   ],
   imports: [
